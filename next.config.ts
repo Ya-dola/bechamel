48025588bd150081a9a1beb9d68000376b3588c3
Yadola <Ya-dola@users.noreply.github.com>
2025-02-02 20:44:01 +0530

    build(next-config): update next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // To generate static pages on build
+  output: 'export',
+
+  // Paths for when Hosting
+  // basePath: '/bechamel',
+  // assetPrefix: '/bechamel',
 };
 
 export default nextConfig;
