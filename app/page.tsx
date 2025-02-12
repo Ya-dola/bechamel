17e0a62a34e71860f78c29c455ccc378f6bdebcb
Yadola <Ya-dola@users.noreply.github.com>
2025-02-12 20:14:11 +0530

    feat(supabase-client): add supabase client
    Added supabase client for testing functions from tables

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-
 import { createClient } from '@supabase/supabase-js';
+import Link from 'next/link';
 
-export default function Home() {
+function Home() {
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
@@ -50,7 +50,7 @@ export default function Home() {
             rel='noopener noreferrer'
           >
             Read our docs
-          </a>{' '}
+          </a>
           <button
             className='rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44'
             onClick={async () => {
@@ -72,6 +72,12 @@ export default function Home() {
           >
             Fetch data
           </button>
+          <Link
+            href='/supabase'
+            className='rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44'
+          >
+            Go To Supa Test
+          </Link>
         </div>
       </main>
       <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'>
@@ -124,3 +130,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
---
+++ app/supabase/page.tsx
@@ -0,0 +1,7 @@
+import SupabaseClient from '@/clients/supabase/supabase_client';
+
+function Supabase() {
+  return <SupabaseClient />;
+}
+
+export default Supabase;
