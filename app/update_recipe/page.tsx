9a59ce68dd81a4d098929b2a659ce483532c4ebd
Yadola <Ya-dola@users.noreply.github.com>
2025-02-17 20:47:52 +0530

    feat(supabase-client): add update logic screen

---
+++ app/update_recipe/page.tsx
@@ -0,0 +1,7 @@
+import UpdateRecipeClient from '@/clients/update_recipe_client/update_recipe_client';
+
+function UpdateRecipe() {
+  return <UpdateRecipeClient />;
+}
+
+export default UpdateRecipe;
