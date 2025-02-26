93e8355a907b499141efa650bc21ab67df536061
Yadola <Ya-dola@users.noreply.github.com>
2025-02-26 22:19:05 +0530

    feat(test-client): add test client

--- clients/testClient/testClient.tsx
+++ clients/testClient/testClient.tsx
@@ -14,6 +14,43 @@ function TestClient() {
         labelColor={'text-slate-700'}
         subTextColor={'text-slate-900'}
       />
+      <IconCard
+        label={'Prep Time'}
+        subText={'5 min'}
+        icon={'line-md:speed'}
+        bgColor={'bg-green-200'}
+        labelColor={'text-slate-700'}
+        subTextColor={'text-slate-900'}
+      />
+      <IconCard
+        label={'Prep Time'}
+        subText={'5 min'}
+        icon={'line-md:speed'}
+        iconSize={'36'}
+        iconColor={'text-emerald-400'}
+        bgColor={'bg-violet-200'}
+        labelColor={'text-slate-700'}
+        subTextColor={'text-slate-900'}
+      />
+      <IconCard
+        label={'Prep Time'}
+        subText={'5 min'}
+        icon={'line-md:speed'}
+        iconSize={'48'}
+        bgColor={'bg-green-200'}
+        labelColor={'text-slate-700'}
+        subTextColor={'text-slate-900'}
+      />
+      <IconCard
+        label={'Prep Time'}
+        subText={'5 min'}
+        icon={'line-md:speed'}
+        iconSize={'128'}
+        iconColor={'text-rose-400'}
+        bgColor={'bg-red-300'}
+        labelColor={'text-slate-700'}
+        subTextColor={'text-slate-900'}
+      />
     </div>
   );
 }
