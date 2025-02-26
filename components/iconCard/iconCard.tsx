102928dc8250e12c614e7b38c526da7651b13ca4
Yadola <Ya-dola@users.noreply.github.com>
2025-02-26 22:18:46 +0530

    feat(icon-card): add icon card

--- components/iconCard/iconCard.tsx
+++ components/iconCard/iconCard.tsx
@@ -1,30 +1,50 @@
+import { Icon } from '@iconify/react';
+
 interface IconCardProps {
   label: string;
   subText?: string;
+  icon?: string;
+  iconColor?: string;
+  iconSize?: string;
   bgColor?: string;
   radius?: string;
   labelStyle?: string;
   labelColor?: string;
   subTextStyle?: string;
   subTextColor?: string;
+  padding?: string;
+  cardGap?: string;
 }
 
 function IconCard({
   label,
   subText,
+  icon,
+  iconColor = 'text-gray-700',
+  iconSize = '24',
   bgColor = 'bg-pink-200',
   radius = 'rounded-lg',
-  labelColor: textColor = 'text-gray-700',
-  labelStyle = `font-medium ${textColor}`,
+  labelColor = 'text-gray-700',
+  labelStyle = `font-medium ${labelColor}`,
   subTextColor = 'text-gray-900',
   subTextStyle = `font-semibold ${subTextColor}`,
+  padding = 'p-3',
+  cardGap = 'gap-1',
 }: IconCardProps) {
   return (
     <div
-      className={`flex flex-col items-center justify-center w-fit
-         gap-1 p-4 fit-content ${bgColor} ${radius}`}
+      className={`flex flex-col items-center justify-center w-fit fit-content ${cardGap} ${padding} ${bgColor} ${radius}`}
     >
-      {/* TODO - Icon */}
+      {icon && (
+        <div className={`aspect-square w-${iconSize} h-${iconSize}`}>
+          <Icon
+            icon={icon}
+            className={`${iconColor} w-full h-full`}
+            height={iconSize}
+            width={iconSize}
+          />
+        </div>
+      )}
 
       <p className={labelStyle}>{label}</p>
 
