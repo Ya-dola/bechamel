e4cb091f60690f084cf702614d6e2c4d9e7e6640
Yadola <Ya-dola@users.noreply.github.com>
2025-02-28 23:08:48 +0530

    feat(test-client): add inputs to test client

--- clients/testClient/testClient.tsx
+++ clients/testClient/testClient.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import DirectionsCards from '@/components/directionsCard/directionsCard';
 import CustomBadge from '@/components/customBadge/customBadge';
 import IconCard from '@/components/iconCard/iconCard';
@@ -9,11 +10,17 @@ import CustomImage from '@/components/customImage/customImage';
 import CustomAppShell from '@/components/customAppShell/customAppShell';
 import RecipeCard from '@/components/recipeCard/recipeCard';
 import NotesCard from '@/components/notesCard/notesCard';
+import CustomTextInput from '@/components/customTextInput/customTextInput';
+import CustomPasswordInput from '@/components/customPasswordInput/customPasswordInput';
 
 function TestClient() {
+  const [textValue, setTextValue] = useState('');
+  const [emailValue, setEmailValue] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <CustomAppShell padding={0}>
-      <div className='flex flex-col p-8 gap-4'>
+      <div className='flex flex-col p-8 gap-4 pb-30'>
         <h1 className='w-full text-center'>Test Client</h1>
         <div className='flex flex-row flex-wrap p-8 gap-4'>
           <LineBookCard />
@@ -70,19 +77,54 @@ function TestClient() {
             text={'With Icon'}
             icon={'line-md:star'}
           />
+          <RecipeCard
+            heading='Pancake'
+            username='Firt name lastname'
+            totalTime={60}
+            difficulty={'hard'}
+            width={300}
+            imageSrc='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png'
+          />
+          <div className='w-fit h-fit'>
+            <h2 className='text-xl font-bold mb-2'>Plain Text Input</h2>
+            <CustomTextInput
+              label='Text Input'
+              placeholder='Enter some text'
+              value={textValue}
+              onValueChange={setTextValue}
+              containerClassName={'w-sm'}
+              clearable
+            />
+          </div>
+          <div className='w-fit h-fit'>
+            <h2 className='text-xl font-bold mb-2'>Email Input</h2>
+            <CustomTextInput
+              type='email'
+              label='Email Input'
+              placeholder='Enter your email'
+              value={emailValue}
+              onValueChange={setEmailValue}
+              containerClassName={'w-md'}
+              clearable
+              withAsterisk
+            />
+          </div>
+          <div className='w-fit h-fit'>
+            <h2 className='text-xl font-bold mb-2'>Password Input</h2>
+            <CustomPasswordInput
+              label={'Password'}
+              placeholder={'Enter your password'}
+              value={password}
+              onValueChange={setPassword}
+              containerClassName={'w-lg'}
+            />
+            <p>Current Password: {password}</p>
+          </div>
+          <CustomImage
+            height={250}
+            imageSrc='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png'
+          />
         </div>
-        <CustomImage
-          height={200}
-          imageSrc='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png'
-        />
-        <RecipeCard
-          heading='Pancake'
-          username='Firt name lastname'
-          totalTime={60}
-          difficulty={'hard'}
-          width={300}
-          imageSrc='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png'
-        />
       </div>
     </CustomAppShell>
   );
