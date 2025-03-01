980c4e1aa62551d79efaa7f1cacff12e4e7d0d7b
Sen-1827 <powderrush9@gmail.com>
2025-03-01 21:51:58 +0530

    feat(recipe-page): add recipe page

---
+++ app/recipe_page/page.tsx
@@ -0,0 +1,7 @@
+import RecipePageClient from '@/clients/recipe_page_client/recipe_page_client';
+
+function RecipePage() {
+  return <RecipePageClient />;
+}
+
+export default RecipePage;
--- components/directionsCard/directionsCard.tsx
+++ components/directionsCard/directionsCard.tsx
@@ -4,7 +4,10 @@ function DirectionsCards() {
   return (
     <LineBookCard
       title={'Directions'}
-      items={['directions 1', 'directions 2']}
+      items={[
+        'm No. 11 of the Fatui Harbingers, codename Childe, but I also go by Tartaglia. And you... Hmm, you too like to cause quite the stir, dont you? Something tells me were going to get along splendidly.',
+        'directions 2',
+      ]}
       displayMode={'steps'}
     />
   );
