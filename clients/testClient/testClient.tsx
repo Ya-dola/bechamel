6dd4e423aba0e31a95359b45a9c5967a611aaab6
Yadola <Ya-dola@users.noreply.github.com>
2025-03-01 15:21:45 +0530

    feat(gradient-card): add gradient card

--- clients/testClient/testClient.tsx
+++ clients/testClient/testClient.tsx
@@ -12,6 +12,7 @@ import RecipeCard from '@/components/recipeCard/recipeCard';
 import NotesCard from '@/components/notesCard/notesCard';
 import CustomTextInput from '@/components/customTextInput/customTextInput';
 import CustomPasswordInput from '@/components/customPasswordInput/customPasswordInput';
+import GradientCard from '@/components/gradientCard/gradientCard';
 
 function TestClient() {
   const [textValue, setTextValue] = useState('');
@@ -85,6 +86,18 @@ function TestClient() {
             width={300}
             imageSrc='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png'
           />
+          <GradientCard>
+            <p>Gradient Card</p>
+          </GradientCard>
+          <GradientCard
+            gradient={
+              'bg-gradient-to-b from-violet-300 from-40% to-emerald-600/50 to-90%'
+            }
+          >
+            <p>Gradient Card</p>
+            <p>With Green Color</p>
+          </GradientCard>
+
           <div className='w-fit h-fit'>
             <h2 className='text-xl font-bold mb-2'>Plain Text Input</h2>
             <CustomTextInput
---
+++ components/gradientCard/gradientCard.tsx
@@ -0,0 +1,23 @@
+interface GradientCardProps {
+  gradient?: string;
+  flex?: string;
+  padding?: string;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+function GradientCard({
+  gradient = 'bg-gradient-to-b from-violet-300 to-rose-400',
+  flex = 'flex flex-col items-center justify-center gap-1',
+  padding = 'px-8 py-4',
+  className = 'h-fit w-fit rounded-xl',
+  children,
+}: GradientCardProps) {
+  return (
+    <div className={`${flex} ${padding} ${gradient} ${className}`}>
+      {children}
+    </div>
+  );
+}
+
+export default GradientCard;
