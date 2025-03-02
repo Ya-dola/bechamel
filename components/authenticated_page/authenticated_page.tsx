621c18ec9982fbf86476df179b44ab427b915efe
Yadola <Ya-dola@users.noreply.github.com>
2025-03-02 17:30:55 +0530

    fix(authenticated-page): fix redirect to sign in

--- components/authenticated_page/authenticated_page.tsx
+++ components/authenticated_page/authenticated_page.tsx
@@ -36,7 +36,7 @@ export default function AuthenticatedPage({
       console.log(
         'AuthenticatedPage: no session after fetch, redirecting to /login',
       );
-      router.push('/login');
+      router.push('/signIn');
     }
   }, [localLoading, session, router]);
 
