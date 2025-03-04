a723faa05b8c6c90f22da010b17e7986c7164579
Yadola <Ya-dola@users.noreply.github.com>
2025-03-04 06:56:59 +0530

    feat(direction-card): add items props

--- components/directionsCard/directionsCard.tsx
+++ components/directionsCard/directionsCard.tsx
@@ -1,15 +1,22 @@
 import LineBookCard from '@/components/lineBookCard/lineBookCard';
 
-function DirectionsCards() {
+interface Direction {
+  instruction: string;
+}
+
+interface DirectionsCardProps {
+  items?: Direction[];
+}
+
+function DirectionsCard({ items = [] }: DirectionsCardProps) {
+  const formattedItems = items.map((dir) => dir.instruction);
   return (
     <LineBookCard
-      title={'Directions'}
-      items={[
-        'm No. 11 of the Fatui Harbingers, codename Childe, but I also go by Tartaglia. And you... Hmm, you too like to cause quite the stir, dont you? Something tells me were going to get along splendidly.',
-        'directions 2',
-      ]}
-      displayMode={'steps'}
+      title='Directions'
+      items={formattedItems}
+      displayMode='steps'
     />
   );
 }
-export default DirectionsCards;
+
+export default DirectionsCard;
