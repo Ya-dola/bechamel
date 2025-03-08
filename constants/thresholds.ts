56ea398869001ac85d48595197927c2f6a01b0cb
Yadola <Ya-dola@users.noreply.github.com>
2025-03-08 15:49:10 +0530

    fix(supabase-service): add proper upload image logic

---
+++ constants/thresholds.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+/**
+ * The maximum allowed image size in megabytes.
+ *
+ * @constant {number} MAXIMAGESIZEMB
+ * @default 0.8
+ */
+const MAXIMAGESIZEMB: number = 0.8 * 1024 * 1024; // Maximum image size in MB
